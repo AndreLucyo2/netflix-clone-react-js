42525f8e33c7221de909f01592d2c122630b2fa2
AndreLucyo <andre.lucio@nextar.com.br>
2022-06-22 23:08:42 -0300

    console.log da lista sendo retornada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useEffect } from 'react';
+import Tmdb from './tmdb.js';
 
-export default()=>{
-  return(
+export default () => {
+
+  //ao atualizar a pagina executa esta função
+  useEffect(() => {
+
+    //Requisitando toda a lista:
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+
+    };
+    loadAll();
+
+  }, [])
+
+  return (
     <div>
-      Olá Mundo! 
+      Olá Mundo!
     </div>
   )
 }
