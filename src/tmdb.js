468942815fd86445ddc5297405e843d0b1210ffc
AndreLucyo <andre.lucio@nextar.com.br>
2022-06-22 22:37:54 -0300

    criar arquivo base da API TMDB

---
+++ src/tmdb.js
@@ -0,0 +1,18 @@
+const API_KEY = '488822f9eadf3c668dfdd998e64474bf'
+const API_BASEurl = 'https://api.themoviedb.org/3'
+
+/*
+- otiginais da netflix
+- recomendados (trending)
+- em alta (top rated)
+- ação
+- comedia
+- terror
+- romance
+- documentários  
+*/
+
+
+export default {
+
+}
