e74e213a77d303020273a6b577b6c85460402629
AndreLucyo <andre.lucio@nextar.com.br>
2022-06-22 23:15:23 -0300

    react salva a lista

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Tmdb from './tmdb.js';
 
 export default () => {
@@ -7,12 +7,17 @@ export default () => {
   //ao atualizar a pagina executa esta função
   useEffect(() => {
 
+    //REACT: Salva alista par apoder utilizar na tels
+    const [movieList, setMovieList] = useState([]);
+
     //Requisitando toda a lista:
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      //console.log(list);
+      setMovieList(list);
 
     };
+    
     loadAll();
 
   }, [])
