b305e89d8c0f4e72ae4dfb39c65deb21f6e9a51f
AndreLucyo <andre.lucio@nextar.com.br>
2022-06-22 22:43:26 -0300

    criado estrutura da listagem do que vai ser pego na API

--- src/tmdb.js
+++ src/tmdb.js
@@ -14,5 +14,52 @@ const API_BASEurl = 'https://api.themoviedb.org/3'
 
 
 export default {
+    getHomeList: async () => {
+        return [
+
+            {
+                slug: 'originals',
+                tetle: 'Originais do Netflix',
+                items: []
+            },
+            {
+                slug: 'trending',
+                tetle: 'Recomendados para você',
+                items: []
+            },
+            {
+                slug: 'toprated',
+                tetle: 'Em Alta',
+                items: []
+            },
+            {
+                slug: 'action',
+                tetle: 'Ação',
+                items: []
+            },
+            {
+                slug: 'comedy',
+                tetle: 'Comedia',
+                items: []
+            },
+            {
+                slug: 'horror',
+                tetle: 'Terror',
+                items: []
+            },
+            {
+                slug: 'romance',
+                tetle: 'Romance',
+                items: []
+            },
+            {
+                slug: 'documentary',
+                tetle: 'Documentários',
+                items: []
+            },
+
+        ]
+
+    }
 
 }
