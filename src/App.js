3e1132ab4941877f42569cdf1ab940401cf486e8
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-27 21:29:11 -0300

    estudo par acorreção ()

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import Tmdb from './tmdb.js';
 import MovieRow from './components/movieRow.js'
 
-export default () => {
-
+function App() {
   //REACT: Salva alista para poder utilizar na tels
   const [movieList, setMovieList] = useState([]);
 
@@ -12,10 +11,12 @@ export default () => {
 
     //Requisitando toda a lista:
     const loadAll = async () => {
-
       let list = await Tmdb.getHomeList();
-      //console.log(list);
       setMovieList(list);
+      
+      //DEBUG:
+      console.log(list);
+      console.log('-------------------------------------');
     };
 
     //Carregar a lista:
@@ -26,11 +27,48 @@ export default () => {
   return (
     <div className="page">
       <section className="lists">
-        {movieList.map((item, key) => (
-          <MovieRow key={key} title={item.title} items={item.items} />
+        {movieList.map((item, key) => (          
+          <MovieRow key={key} titulo={item.title} lsFilmes ={item.items}/>          
         ))}
       </section>
     </div>
   );
 
 }
+
+export default App;
+
+// export default () => {
+
+//   //REACT: Salva alista para poder utilizar na tels
+//   const [movieList, setMovieList] = useState([]);
+
+//   //ao atualizar a pagina executa esta função
+//   useEffect(() => {
+
+//     //Requisitando toda a lista:
+//     const loadAll = async () => {
+//       let list = await Tmdb.getHomeList();
+//       setMovieList(list);
+      
+//       //DEBUG:
+//       console.log(list);
+//       console.log('-------------------------------------');
+//     };
+
+//     //Carregar a lista:
+//     loadAll();
+
+//   }, [])
+
+//   return (
+//     <div className="page">
+//       <section className="lists">
+//         {movieList.map((item, key) => (          
+//           <MovieRow key={key} titulo={item.title} lsFilmes ={item.items}/>          
+//         ))}
+//       </section>
+//     </div>
+//   );
+
+// }
--- src/components/movieRow.js
+++ src/components/movieRow.js
@@ -1,18 +1,39 @@
 import React from "react";
 import './movieRow.css'
 
-//recebe as prop. titulo e o array de itens
-export default ({title, items}) => {
-    
+const MovieRow = ({ titulo, lsFilmes }) => {
+    //console.log(lsFilmes)
+    //console.log('json: ' + lsFilmes)
+    //console.log('log row: ' + lsFilmes.results.length)
+    //console.log('log img: ' + lsFilmes[0].vote_count)
+
     return (
         <div>
-            <h2>{title}</h2>
+            <h2>Titulo: {titulo}</h2>
             <div className="movieRow--listarea">
-                {items.results.legth > 0 && items.results.map((item,key)=>(
-                    <img src=''/>
+                {lsFilmes.results.map((item, key) => (
+                    <p key={item.id}>
+                        <h1>{item.poster_path}</h1>
+                    </p>
+
                 ))}
-                
             </div>
         </div>
     )
 };
+
+//recebe as prop. titulo e o array de itens
+export default MovieRow;
+
+//<h4 key={key}>{filme.poster_path}</h4>
+//<img src={`https://image.tmdb.org/t/p/w300${filme.poster_path}`} alt={filme.original_title} key={key} />
+
+/* <div className="movieRow--listarea">
+{items.results.legth > 0 && items.results.map((item, key) => (
+    <h2>{title}</h2>
+
+))}
+</div> */
+
+
+//<img src={`https://image.tmdb.org/t/p/w300${item.items.poster_path}`} alt={item.original_title} key={key} />
